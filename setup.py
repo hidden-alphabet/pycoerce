86b66cd2377c567f8eb9ee0854ae65f8a7c69f23
Cole Hudson <cole@colejhudson.com>
2019-07-02 17:00:48 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='pycoerce',
+    version='0.0.1',
+    author=["Cole Hudosn"],
+    author_email="cole@colejhudson.com",
+    description="Encode python objects into other type systems (e.g. SQL, PyArrow, etc)",
+    install_requires=[ "pyarrow" ],
+    packages=setuptools.find_packages()
+)
