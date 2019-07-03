f6fac2eced9271f8278ee1fdbcb44792ec0abc3d
Cole Hudson <cole@colejhudson.com>
2019-07-02 17:30:18 -0700

    conformed file to pep8 styling

--- pycoerce/postgresql.py
+++ pycoerce/postgresql.py
@@ -1,16 +1,33 @@
+# pylint: disable=dangerous-default-value,too-few-public-methods
+
+'''
+Description
+-----------
+Extract PostgreSQL data types from Python objects.
+
+Example
+-------
+>>> import pycoerce.postgresql as py2pg
+>>> columns = py2pg.dumps({ "id": 0, "name": "foo bar" })
+>>> table = ", ".join([ "{} {}".format(name, sqltype) for name, sqltype in columns.items()])
+>>> stmt = "CREATE TABLE example({});".format(table)
+>>> stmt == "CREATE TABLE example(id integer, name text);"
+True
+'''
+
 from collections.abc import Iterable
 
-# By default, encountering these types will cause the program to 
-# throw an exception as most SQL implementations don't allow for 
-# columns with nested data types. By implementing your own decoder, 
+# By default, encountering these types will cause the program to
+# throw an exception as most SQL implementations don't allow for
+# columns with nested data types. By implementing your own decoder,
 # you can override this behavior.
-NESTED = [ range, iter, list, dict, tuple, set, frozenset ]
+NESTED = [range, iter, list, dict, tuple, set, frozenset]
 
 # These python types will cause an exception to be thrown if they're
 # encountered without a decoder being given to handle them. This is the
 # case for any python type for which there is no obvious default into
 # corresponding type in SQL.
-UNSUPPORTED = [ complex ]
+UNSUPPORTED = [complex]
 
 class PostgresTypeEncoder:
     '''
@@ -22,9 +39,14 @@ class PostgresTypeEncoder:
     '''
 
     def __init__(self, hooks={}):
-        # https://www.postgresql.org/docs/8.4/datatype.html 
+        # https://www.postgresql.org/docs/8.4/datatype.html
         if not isinstance(hooks, dict):
-            raise TypeError("The 'hooks' argument must be a dictionary from classes to, either strings or callables.")
+            raise TypeError(
+                '''
+                The 'hooks' argument must be a dictionary from classes to, either
+                strings or callables.
+                '''
+            )
 
         self.hooks = {
             int: 'integer',
@@ -35,58 +57,50 @@ class PostgresTypeEncoder:
             float: 'real',
             str: 'text',
             **hooks,
-            None: self._reject_type,
-            type(None): self._reject_type,
-
         }
-    
-    def _reject_type(self, t):
-        raise TypeError('SQL does not have a type corresponding to {}.'.format(t))
-
-    def _get_object_types(self, obj):
-        return [type(item) for item in obj]
 
     def _resolve_hook(self, pytype):
         resolver = self.hooks[pytype]
 
         if isinstance(resolver, str):
             return resolver
-        elif callable(resolver):
+
+        if callable(resolver):
             return resolver(pytype)
-        else:
-            raise TypeError(
-                '''
-                Hooks must be either strings or callable. The hook given for
-                python objects of type '{}' is neither.
-                '''.format(pytype)
-            )
+
+        raise TypeError(
+            '''
+            Hooks must be either strings or callable. The hook given for
+            python objects of type '{}' is neither.
+            '''.format(pytype)
+        )
 
     def _validate_object_types(self, obj):
-        types = self._get_object_types(obj)
+        types = [type(item) for item in obj]
 
-        for t in types:
-            if t in self.hooks:
-                continue
+        for pytype in types:
+            if pytype not in self.hooks:
+                raise TypeError('SQL does not have a type corresponding to {}.'.format(pytype))
 
-            if t in NESTED:
+            if pytype in NESTED:
                 raise TypeError(
                     '''
-                    The type '{}' is nested. Nested types aren't valid in PostgreSQL. 
+                    The type '{}' is nested. Nested types aren't valid in PostgreSQL.
                     You should either flatten the corresponding object of that type
                     into it's container object, or write a decoder to do so dynamically.
-                    '''.format(t)
+                    '''.format(pytype)
                 )
 
-            if t in UNSUPPORTED:
+            if pytype in UNSUPPORTED:
                 raise TypeError(
                     '''
                     The object of type '{}' has no default corresponding type within PostgerSQL.
                     You can resolve this by either removing the object, aliasing it to an
                     PostgreSQL type, or writing a decoder.
-                    '''.format(t)
+                    '''.format(pytype)
                 )
 
-        return types 
+        return types
 
     def _iter_to_sql_types(self, obj):
         types = self._validate_object_types(obj)
@@ -94,31 +108,47 @@ class PostgresTypeEncoder:
 
     def _dict_to_sql_columns(self, obj):
         types = self._validate_object_types(obj.values())
-        return { k: self._resolve_hook(t) for k, t in zip(obj.keys(), types) }
+        return {k: self._resolve_hook(t) for k, t in zip(obj.keys(), types)}
 
     def encode(self, obj):
+        '''
+        Description
+        -----------
+        Extracts the SQL datatypes from a python object.
+
+        Example
+        -------
+        >>> import pycoerce.postgresql as py2pg
+        >>> encoder = py2pg.PostgresTypeEncoder()
+        >>> encoder.encode(1)
+        'integer'
+        >>> encoder.encode({ "is_admin": True })
+        {'is_admin': 'boolean'}
+        '''
         obj_t = type(obj)
 
         if obj_t in self.hooks:
             return self._resolve_hook(obj_t)
-        elif obj_t is dict:
+
+        if obj_t is dict:
             return self._dict_to_sql_columns(obj)
-        elif isinstance(obj, Iterable):
+
+        if isinstance(obj, Iterable):
             return self._iter_to_sql_types(obj)
-        else:
-            raise TypeError(
-                '''
-                PostgresTypeEncoder cannot encode objects of type '{}' to an SQL
-                equivalent 
-                '''
-            )
+
+        raise TypeError(
+            '''
+            PostgresTypeEncoder cannot encode objects of type '{}' to an SQL
+            equivalent
+            '''
+        )
 
 def dumps(obj, hooks={}):
     '''
     Description
     -----------
     Converts an arbitrary python object to the corresponding
-    type representation in SQL. 
+    type representation in SQL.
 
     Usage
     -----
