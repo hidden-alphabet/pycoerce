7f6f4cea575fd4556b223ae0dda0f22b62be37af
Cole Hudson <cole@colejhudson.com>
2019-07-02 18:45:12 -0700

    fixed pep8 styling

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setuptools.setup(
     author=["Cole Hudosn"],
     author_email="cole@colejhudson.com",
     description="Encode python objects into other type systems (e.g. SQL, PyArrow, etc)",
-    install_requires=[ "pyarrow" ],
+    install_requires=["pyarrow"],
     packages=setuptools.find_packages()
 )
